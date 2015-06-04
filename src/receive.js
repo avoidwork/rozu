d81471c094e764a86a57ff3c486b691dc0935ba5
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-06-03 23:12:39 -0400

    Disabling bearer token auth as it's not needed, hardening validation of `token` on `/receive` by testing for the `host` allowing for reasonable 1 factor trust & easy testing
    Removing hardcoded testing variable

--- src/receive.js
+++ src/receive.js
@@ -7,9 +7,10 @@
  * @return {Undefined}  undefined
  */
 function receive ( req, res ) {
-	var token = req.parsed.query.token;
+	var token = req.parsed.query[config.token],
+		record = token ? stores.webhooks.get( token ) : undefined;
 
-	if ( !token || stores.webhooks.indexes.key[ token ] === undefined ) {
+	if ( !token || !record || record.data.host.indexOf( req.parsed.hostname ) === -1 ) {
 		res.error( 401 );
 	} else if ( req.body === undefined || !regex.payload.test( typeof req.body ) ) {
 		res.error( 400 );
