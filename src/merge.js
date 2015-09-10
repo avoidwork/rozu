e940c396e72739b4c19edc9fc07872944346ecde
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-09-09 21:26:39 -0400

    Updating deps to ensure compatibility with nodejs 4.0.0

--- src/merge.js
+++ src/merge.js
@@ -10,17 +10,17 @@ function merge (a, b) {
 	let c = a !== undefined ? clone(a) : a,
 		d = b !== undefined ? clone(b) : b;
 
-	if ((c instanceof Object) && (d instanceof Object)) {
+	if (c instanceof Object && b instanceof Object) {
 		Object.keys(d).forEach(function (i) {
-			if ((c[i] instanceof Object) && (d[i] instanceof Object)) {
+			if (c[i] instanceof Object && d[i] instanceof Object) {
 				c[i] = merge(c[i], d[i]);
-			} else if ((c[i] instanceof Array) && (d[i] instanceof Array)) {
+			} else if (c[i] instanceof Array && d[i] instanceof Array) {
 				c[i] = c[i].concat(d[i]);
 			} else {
 				c[i] = d[i];
 			}
 		});
-	} else if ((c instanceof Array) && (d instanceof Array)) {
+	} else if (c instanceof Array && d instanceof Array) {
 		c = c.concat(d);
 	} else {
 		c = d;
