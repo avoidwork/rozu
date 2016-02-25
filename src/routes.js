d4b398b76115c2738287da77fbbdd0a025f05417
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-02-24 19:08:37 -0500

    Adding COAP support (WIP)

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+const http = require("http");
+
 /**
  * API routes
  *
@@ -15,7 +17,7 @@ const routes = {
 		}
 	},
 	"get": {
-		"/": function (req, res) {
+		"/": (req, res) => {
 			let session = req.session,
 				headers;
 
@@ -28,7 +30,7 @@ const routes = {
 				res.respond(["login", "receive", "register"]);
 			}
 		},
-		"/admin": function (req, res) {
+		"/admin": (req, res) => {
 			let luser = req.session.passport.user;
 
 			if (array.contains(config.admin, luser.email)) {
@@ -49,11 +51,13 @@ const routes = {
 			"instruction": config.instruction.send
 		},
 		"/stream": sse.init,
-		"/users": function (req, res) {
+		"/users": (req, res) => {
 			if (req.session.admin) {
-				res.respond(stores.users.dump().map(function (i) {
-					delete i.password;
-					return i;
+				res.respond(stores.users.dump().map(i => {
+					let o = i;
+
+					delete o.password;
+					return o;
 				}));
 			} else {
 				res.error(403);
@@ -62,16 +66,16 @@ const routes = {
 		"/users/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": user,
 		"/verify": config.instruction.verify_endpoint,
 		"/verify/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": verify,
-		"/webhooks": function (req, res) {
+		"/webhooks": (req, res) => {
 			collection(req, res, "webhooks");
 		},
-		"/webhooks/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": function (req, res) {
+		"/webhooks/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": (req, res) => {
 			collection_item(req, res, "webhooks");
 		}
 	},
 	patch: {
 		"/profile": profile,
-		"/webhooks/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": function (req, res) {
+		"/webhooks/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": (req, res) => {
 			collection_item(req, res, "webhooks", validation.webhooks);
 		}
 	},
@@ -79,14 +83,36 @@ const routes = {
 		"/register": register,
 		"/receive": receive,
 		"/send": send,
-		"/webhooks": function (req, res) {
+		"/webhooks": (req, res) => {
 			collection(req, res, "webhooks", validation.webhooks);
 		}
 	},
 	put: {
 		"/profile": profile,
-		"/webhooks/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": function (req, res) {
+		"/webhooks/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}": (req, res) => {
 			collection_item(req, res, "webhooks", validation.webhooks);
+		}
+	},
+	coap: {
+		request: (req, res, coapServer, tensoServer) => {
+			let method = req.method,
+				result, status;
+
+			if (method === "GET") {
+				status = 200;
+				result = tensoServer.serialize(undefined, {
+					"instruction": config.instruction.receive
+				});
+			} else if (method === "POST") {
+				status = 202;
+				result = tensoServer.serialize(undefined, http.STATUS_CODE[status]);
+			} else {
+				status = 405;
+				result = tensoServer.serialize(undefined, new Error(status));
+			}
+
+			res.writeHead(status, {"Content-Format": "application/json"});
+			res.send(result);
 		}
 	}
 };
