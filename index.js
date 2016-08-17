dd2a5aac3791cbdf58b7bf40dff87534e831690c
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-08-17 19:07:01 -0400

    More refactoring (WIP)

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-'use strict';
+"use strict";
 
-module.exports = () => true;
+const fs = require("fs"),
+	path = require("path"),
+	merge = require("tiny-merge"),
+	root = __dirname,
+	cfg = require(path.join(root, "config.json")),
+	clone = require(path.join(root, "lib", "clone.js")),
+	rozu = require(path.join(root, "lib", "rozu.js"));
+
+function factory (arg) {
+	let hostname = arg ? arg.hostname || "localhost" : "localhost",
+		hosts = {},
+		config = arg ? merge(clone(cfg), arg) : clone(cfg),
+		obj;
+
+	if (!config.port) {
+		console.error("Invalid configuration");
+		process.exit(1);
+	}
+
+	hosts[hostname] = "www";
+	config.root = root;
+	config.hosts = hosts;
+	config.default = hostname;
+	config.template = fs.readFileSync(config.template || path.join(config.root, "template.html"), {encoding: "utf8"});
+	obj = tenso(config);
+	obj.hostname = hostname;
+	utility.bootstrap(obj, config);
+	obj.server.start(config);
+
+	return obj;
+}
+
+module.exports = factory;
